bc2a0cef560584ceb6d1a226783153887444d444
caseywatts <casey.s.watts@gmail.com>
2014-02-22 15:30:32 -0500

    adding more locations

--- hello.rb
+++ hello.rb
@@ -3,3 +3,11 @@ require 'sinatra'
 get '/hi' do
   "Hello World!"
 end
+
+get '/noway' do
+  "NO WAY!"
+end
+
+get '/trololololo' do
+  "Trolololoolololololololololololololololololololololololololloololollololololololololo"
+end
